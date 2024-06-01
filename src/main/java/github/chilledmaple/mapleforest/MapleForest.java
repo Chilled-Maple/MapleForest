2c3e55bafc1b5b3fda9cde96bef29a7dea862bda
ChilledMaple <chilledmaple@gmail.com>
2024-06-01 06:29:05 -0700

    my first commit

---
+++ src/main/java/github/chilledmaple/mapleforest/MapleForest.java
@@ -0,0 +1,8 @@
+package github.chilledmaple.mapleforest;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MapleForest.MODID)
+public class MapleForest {
+    public static final String MODID = "mapleforest";
+}
